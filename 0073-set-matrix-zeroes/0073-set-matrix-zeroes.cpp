62a37fdf7d0d46c3c78045f26e5838d9704fafea
Srajan Shrivastava <121538686+Srajan-02@users.noreply.github.com>
2026-07-15 15:48:44 +0530

    Time: 0 ms (100%), Space: 20.8 MB (50.82%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,60 @@
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         int rows = matrix.size();
+//         int cols = matrix[0].size();
+//         vector<int> zeroRows;
+//         vector<int> zeroCols;
+//         // First pass - store all the rows and columns having zero
+//         for (int row = 0; row < rows; row++) {
+//             for (int col = 0; col < cols; col++) {
+//                 if (matrix[row][col] == 0) {
+//                     zeroRows.push_back(row);
+//                     zeroCols.push_back(col);
+//                 }
+//             }
+//         }
+//         // Second pass - change the row to zero
+//         for (int row : zeroRows) {
+//             for (int col = 0; col < cols; col++) {
+//                 matrix[row][col] = 0;
+//             }
+//         }
+//         // Third pass - change the column to zero
+//         for (int col : zeroCols) {
+//             for (int row = 0; row < rows; row++) {
+//                 matrix[row][col] = 0;
+//             }
+//         }
+//     }
+// };
+
+// OPTIMIZED SOLUTION
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int rows = matrix.size();
         int cols = matrix[0].size();
-        vector<int> zeroRows;
-        vector<int> zeroCols;
-        // First pass - store all the rows and columns having zero
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (matrix[row][col] == 0) {
-                    zeroRows.push_back(row);
-                    zeroCols.push_back(col);
+
+        vector<bool> zeroRow(rows, false);
+        vector<bool> zeroCol(cols, false);
+
+        // Mark rows and columns
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRow[i] = true;
+                    zeroCol[j] = true;
                 }
             }
         }
-        // Second pass - change the row to zero
-        for (int row : zeroRows) {
-            for (int col = 0; col < cols; col++) {
-                matrix[row][col] = 0;
-            }
-        }
-        // Third pass - change the column to zero
-        for (int col : zeroCols) {
-            for (int row = 0; row < rows; row++) {
-                matrix[row][col] = 0;
+
+        // Set zeros in rows and columns where condition is True
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (zeroRow[i] || zeroCol[j]) {
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
