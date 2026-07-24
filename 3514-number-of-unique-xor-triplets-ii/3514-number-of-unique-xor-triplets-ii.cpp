5e182b46fcb75a682f114f03659de1a25b3cd01a
Srajan Shrivastava <121538686+Srajan-02@users.noreply.github.com>
2026-07-24 19:34:28 +0530

    Time: 2497 ms (7.3%), Space: 98.4 MB (39.89%) - LeetHub

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        // unordered_set<int> uniqueOutputs;
+        // int n = nums.size();
+        // // Iterating through all possible triplet index combinations
+        // for (int i = 0; i < n; ++i) {
+        //     for (int j = 0; j < n; ++j) {
+        //         for (int k = 0; k < n; ++k) {
+        //             int xorSum = nums[i] ^ nums[j] ^ nums[k];
+        //             uniqueOutputs.insert(xorSum);
+        //         }
+        //     }
+        // }
+        // // return the size of set i.e count
+        // return uniqueOutputs.size();
+        unordered_set<int> pairXor;
+        unordered_set<int> ans;
+        int n = nums.size();
+        // Store all XORs of two elements
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                pairXor.insert(nums[i] ^ nums[j]);
+            }
+        }
+        // XOR each pair result with every element
+        for (int x : pairXor) {
+            for (int num : nums) {
+                ans.insert(x ^ num);
+            }
+        }
+        return ans.size();
+    }
+};
